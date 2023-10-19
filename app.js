b604649286f3d4fb3576ccd9954536668e2d2ad3
noobax <nerdz78@gmail.com>
2023-10-19 11:48:20 +0300

    complete simple todo list rewrite

--- app.js
+++ app.js
@@ -1,83 +1,70 @@
-import { createServer } from 'node:http'
+import { writeFileSync, existsSync } from 'node:fs'
 import { readFile, writeFile } from 'node:fs/promises'
+import { createServer } from 'node:http'
 import { json } from 'node:stream/consumers'
-import { existsSync, writeFileSync } from 'node:fs'
 
-const getTodos = async (file) => {
-	let todos = []
-	const todoListCsv = await readFile('todos.csv', 'utf-8')
-	todos = todoListCsv.split('\n')
-//	todos.pop()
-
-	return todos.map((e) => JSON.parse(e))
-}
-
-const addTodo = async (file, todo) => {
-	const items = await getTodos(file)
-	const item = {id: (Date.now()), task: todo, done: false}
-
-	items.push(item)
-	writeFile(file, items.map((e) => JSON.stringify(e)).join('\n'))
-}
-
-const editTodo = async (file, id, todoDone) => {
-	const items = await getTodos(file)
-	let newTodo = {}
-
-	for (let item of items) {
-		console.log(item['id'] == id)
-		if (item['id'] == id) {
-			item['done'] = todoDone['done'] === true ? true : false
-			newTodo = item
-		}
-	}
-	writeFile(file, items.map((e) => JSON.stringify(e)).join('\n'))
-	return newTodo
-}
-
-const removeTodo = async (file, id) => {
-	const items = await getTodos(file)
-
-	
-	const newItems = items.filter((item) => item['id'] != id)
-	console.log(newItems)
-	writeFile(file, newItems.map((e) => JSON.stringify(e)).join('\n'))
-	return newItems
-}
-
-
-createServer(async (req, res) => {
-
-	res.writeHead(200, { 'Content-Type': 'application/json' })
-	const url = new URL(req.url, 'http://localhost:3000')
-	const todoFile = 'todos.csv'
-	/*
+const todoFile = 'todo.csv'
+const main = () => {
 	if (!existsSync(todoFile))
-		writeFileSync(todoFile, '', {flag:'w+'})
-	*/
-	if (url.pathname === '/todos') {
-		switch (req.method) {
-			case 'GET':
-				res.end(JSON.stringify(await getTodos(todoFile)))
-				break;
-			case 'POST':
-				const data = await json(req)
-				addTodo(todoFile, data)
-				res.end(JSON.stringify(data))
-				break;
-			case 'PUT':
-				const data2 = await json(req)
-				const id = url.searchParams.get('id')
-				const newTodo = await editTodo(todoFile, id, data2)
-				res.end(JSON.stringify(newTodo))
-				break
-			case 'DELETE':
-				const id2 = url.searchParams.get('id')
-				removeTodo(todoFile, id2)
-				res.end('done')
-				break;
-			default:
-				break;
+		writeFileSync(todoFile, '')
+
+	createServer( async (req, res) => {
+		res.writeHead(200, { 'Content-Type': 'application/json'})
+		const url = new URL(req.url, 'http://localhost:3000')
+			if (url.pathname === '/todos') {
+			switch (req.method) {
+				case 'GET':
+					const todos = await getTodos()
+					res.end(JSON.stringify(todos))
+					break
+				case 'POST':
+					const task = await json(req)
+					addTodo(task)
+					break
+				case 'PUT':
+					const toggle = await json(req)
+					toggleTodo(parseInt(url.searchParams.get('id')), toggle)
+					break
+				case 'DELETE':
+					removeTodo(parseInt(url.searchParams.get('id')))
+					break
+			}
 		}
-	}
-}).listen(3000, 'localhost')
+		res.end()
+	}).listen(3000, 'localhost')
+}
+main()
+
+const getTodos = async () => {
+	const todos = await readFile(todoFile, 'utf-8')
+	if (todos === '')
+		return []
+	return todos.split('\n').map((todo) => JSON.parse(todo))
+}
+
+const addTodo = async (task) => {
+	const todos = await getTodos()
+	const id = Date.now()
+
+	todos.push({id: id, title: task.title, completed: false})
+
+	writeFile(todoFile, todos.map((todo) => JSON.stringify(todo)).join('\n'))
+}
+
+const toggleTodo = async (id, toggle) => {
+	const todos = await getTodos()
+	todos.map((todo) => {
+		if (todo.id === id) todo.completed = toggle.completed === true ? true : false
+		return todo
+	})
+
+	writeFile(todoFile, todos.map((todo) => JSON.stringify(todo)).join('\n'))
+}
+
+const removeTodo = async (id) => {
+	const todos = await getTodos()
+
+	writeFile(todoFile, todos.filter((todo) => todo.id !== id)
+														.map((todo) => JSON.stringify(todo))
+														.join('\n'))
+}
